cc7b1fdbf89aab9cbf30a26d1476d42741058291
jeffdavidson343 <jeffreydavidson343@gmail.com>
2023-10-03 19:44:12 -0400

    finising flask setup, geoJSON setup, markerclusters

--- app.py
+++ app.py
@@ -1,101 +1,64 @@
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import create_engine
 from flask import Flask, jsonify
 from flask_cors import CORS
+import json
 
+# from pathlib import Path
+from sqlalchemy import create_engine, inspect
+import pandas as pd
 
 #################################################
 # Database Setup
 #################################################
+
 engine = create_engine("sqlite:///database.sqlite")
 
-
-# # reflect an existing database into a new model
-Base = declarative_base()
-class Brewery(Base):
-    __tablename__ = "breweries"
-    id = Column(String(50), primary_key=True)
-    name = Column(String(50))
-    brewery_type = Column(String(50))
-    address_1 = Column(String(50))
-    city = Column(String(50))
-    state_province = Column(String(50))
-    postal_code = Column(String(50))
-    country =Column(String(50))
-    longitude = Column(Integer)
-    latitude = Column(Integer)
-    phone = Column(Integer)
-    website_url = Column(String(50))
-
-# reflect the tables
-conn = engine.connect()
-Base.metadata.create_all(engine)
-
-# Save reference to the table
-breweries = Base.classes.Brewery
-
-
-#################################################
+################################################
 # Flask Setup
-#################################################
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+################################################
 
-#################################################
-# Flask Routes
-#################################################
+app = Flask (__name__)
+CORS(app)
 
 @app.route("/")
-# @cross_origin()
-
-# def index():
-#     return render_template('index.html')
-
 def welcome():
-    """It worked! List all available api routes."""
+    """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/brewery_data"
+        f"/api/v1.0/locations<br/>"
+        f"api/v1.0/geoJSON<br/>"
     )
 
-@app.route("/api/v1.0/brewery_data")
-def data():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+@app.route("/api/v1.0/locations")
+def locations():
+    with engine.connect() as conn:
+        result = conn.execute('SELECT * FROM database')
 
-    """Return a list of all brewery names"""
-    # Query all breweries
-    results = session.query( id.name, breweries.name, breweries.brewery_type, breweries.address_1, breweries.city, breweries.state, breweries.postal_code, breweries.country, breweries.phone, breweries.website_url).all()
-    session.close()
+        data = [dict(row) for row in result]
 
-# Create a dictionary from the row data and append to a list of all_breweries
-    all_breweries = []
-    for  id, name, brewery_type, address_1, city, state, postal_code, country, phone, website_url, in results:
-       
-        breweries_dict = {}
+    return jsonify(data)
 
-        breweries_dict["id"] = id
-        breweries_dict["name"] = name
-        breweries_dict["brewery_type"] = brewery_type
-        breweries_dict["address_1"] = address_1
-        breweries_dict["city"] = city
-        breweries_dict["state"] = state
-        breweries_dict["postal_code"] = postal_code
-        breweries_dict["country"] = country
-        breweries_dict["phone"] = phone
-        breweries_dict["website_url"] = website_url
+@app.route("/api/v1.0/geoJSON")
+def geoJsonify():
+    with engine.connect() as conn:
+        result = conn.execute('SELECT * FROM database')
 
-        all_breweries.append(breweries_dict)
+        geoData = [dict(row) for row in result]
 
-    return jsonify(all_breweries)
+        geoJson = {
+            "type": "FeatureCollection",
+            "features": [
+            {
+                "type": "Feature",
+                "geometry" : {
+                    "type": "Point",
+                    "coordinates": [d["longitude"], d["latitude"]],
+                    },
+                "properties" : d,
+            } for d in geoData if (d['longitude'] != '' and d['latitude'] != '')]
+        }
 
+    return jsonify(geoJson)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,28 +1,41 @@
+// Creating the map object
 let myMap = L.map("map", {
-    center: [-32.8, 117.9],
-    zoom: 7
-  });
+  center: [37, -95],
+  zoom: 5
+});
 
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-  let brew_json = "../../brewery.json";
-  d3.json(brew_json).then(function(response) {
-    console.log(response);
-    id = response.id;
-   
-    let heatArray = [];
-    for (let i = 0; i < features_length; i++) {
-      let location = features[i].geometry;
-      if (location) {
-        //console.log(location);
-        heatArray.push([location.coordinates[1], location.coordinates[0]]);
-      }
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Store the API query variables.
+
+let url = "http://127.0.0.1:5000/api/v1.0/geoJSON";
+
+// confirm data import
+d3.json(url).then(function(response) {
+
+  // Create a new marker cluster group.
+  let markers = L.markerClusterGroup();
+
+  // Loop through the data.
+  for (let i = 0; i < response.length; i++) {
+
+    // Set the data location property to a variable.
+    let features = response[i].features;
+
+    // Check for the location property.
+    if (features) {
+
+      // Add a new marker to the cluster group, and bind a popup.
+      markers.addLayer(L.marker([features.coordinates[1], features.coordinates[0]])
+        .bindPopup(response[i].descriptor));
     }
-    let heat = L.heatLayer(heatArray, {
-      radius: 20,
-      blur: 35
-    }).addTo(myMap);
-  });
+
+  }
+
+  // Add our marker cluster layer to the map.
+  myMap.addLayer(markers);
+  
+});
